98e6719a5a4c6659d17f16542dacd2d1602cb9cd
Cheng Vue <cvu3@ucsc.edu>
2018-12-23 23:30:45 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,7 +34,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, SUM(pledges.amount)
   FROM users
   JOIN pledges ON users.id = pledges.user_id
-  GROUP BY (user.id)
+  GROUP BY (users.id)
   ORDER BY (pledges.amount AND users.name);"
 end
 
