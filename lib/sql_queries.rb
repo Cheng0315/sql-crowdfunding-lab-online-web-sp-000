0d1d58b04d0bc4c3fcea74dfdd3bb47d6d869624
Cheng Vue <cvu3@ucsc.edu>
2018-12-23 23:30:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,7 +34,8 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, SUM(pledges.amount)
   FROM users
   JOIN pledges ON users.id = pledges.user_id
-  GROUP BY (pledges.amount AND users.name)"
+  GROUP BY (user.id)
+  ORDER BY (pledges.amount AND users.name)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
