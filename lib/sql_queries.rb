fbd785f445f010eb53dd53a00a62f72c72e7e4c4
Cheng Vue <cvu3@ucsc.edu>
2018-12-23 23:12:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT user.name, user.age, SUM(pledges.amount)
   FROM users
   JOIN pledges ON users.id = pledges.user_id
-  GROUP BY (user.id)
+  GROUP BY (pledges.user_id)
   ORDER BY (user.name)"
 end
 
